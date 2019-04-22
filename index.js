da1aebaf65d2a30665cfed9e5e4f348fca318f61
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-22 10:37:56 -1000

    challenge completed

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 function scaleBalance(strArr) {
+  // not an array
+  if (!Array.isArray(strArr)) {
+    throw new Error('Not an array!');
+  }
 
+  // does not contain two elements
+  if (strArr.length !== 2) {
+    throw new Error('Needs to have 2 elements!');
+  }
+
+  // not every element of array is a string
+  const bothStrings = strArr.every(function(elem) {
+    return typeof elem === 'string';
+  });
+  if (!bothStrings) {
+    throw new Error('Needs to be an array of strings!');
+  }
+
+  // parse input arrays
+  const BalArr = JSON.parse(strArr[0]);
+  const weightsArr = JSON.parse(strArr[1]);
+
+  // weights for balancing is an empty array
+  if (weightsArr.length === 0) {
+    throw new Error('2nd element cannot be empty!');
+  }
+
+  // scale and weights are not all positive integers
+  const allPositive = BalArr.concat(weightsArr).every(function(elem) {
+    return elem > 0;
+  });
+
+  if (!allPositive) {
+    throw new Error('Weights need to be positive integers!');
+  }
+
+  const weightNeeded = BalArr[0] - BalArr[1];
+  const absWeightNeeded = Math.abs(weightNeeded);
+
+  let addCheck = 0;
+  let minusCheck = 0;
+
+  let returnVal = 0;
+
+  for (let i = 0; i < weightsArr.length; i++) {
+    // first check whether given element resolves the difference
+    if (weightsArr[i] === absWeightNeeded) {
+      return `0,${weightsArr[i].toString()}`;
+    }
+
+    // next check whether pairing with any remaining elements resolves the difference
+    for (let j = i + 1; j < weightsArr.length; j++) {
+      addCheck = Math.abs(weightsArr[i] + weightsArr[j]);
+      minusCheck = Math.abs(weightsArr[i] - weightsArr[j]);
+      if (addCheck === absWeightNeeded || minusCheck === absWeightNeeded) {
+        returnVal = `${Math.min(weightsArr[i], weightsArr[j]).toString()},${Math.max(
+          weightsArr[i],
+          weightsArr[j],
+        ).toString()}`;
+      }
+    }
+  }
+
+  if (returnVal !== 0) {
+    return returnVal;
+  } else {
+    return 'Not possible';
+  }
 }
 
 module.exports = scaleBalance;
--- test/spec.js
+++ test/spec.js
@@ -1,1 +1,64 @@
 // write tests here
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+const should = chai.should();
+var scaleBalance = require('../index.js');
+
+describe('Input', function() {
+  it('should throw an error if weights are not in an array', function() {
+    expect(function() {
+      scaleBalance('[2, 4], [6, 8]');
+    }).to.throw('Not an array!');
+  });
+
+  it('should throw an error if strArr does not contain 2 elements', function() {
+    expect(function() {
+      scaleBalance(['[2, 5], [3, 4, 6]']);
+    }).to.throw('Needs to have 2 elements!');
+  });
+
+  it('should throw an error if weights are not stored as strings', function() {
+    expect(function() {
+      scaleBalance([[2, 4], [6, 8]]);
+    }).to.throw('Needs to be an array of strings!');
+  });
+
+  it('should throw an error if 2nd element is empty', function() {
+    expect(function() {
+      scaleBalance(['[2, 4]', '[]']);
+    }).to.throw('2nd element cannot be empty!');
+  });
+
+  it('should throw an error if weights are not positive integers', function() {
+    expect(function() {
+      scaleBalance(['[2, 4]', '[1, 5, -8]']);
+    }).to.throw('Weights need to be positive integers!');
+  });
+});
+
+describe('Output', function() {
+  it(`should return "Not possible" if no solution`, function() {
+    let input = ['[1, 9]', '[3, 4, 2]'];
+    let results = scaleBalance(input);
+    expect(results).to.equal('Not possible');
+  });
+
+  it('should add two weights to single side if necessary', function() {
+    let input = ['[1, 9]', '[3, 5, 2]'];
+    let results = scaleBalance(input);
+    expect(results).to.equal('3,5');
+  });
+
+  it('should always use single weight when possible', function() {
+    let input = ['[1, 5]', '[2, 6, 4]'];
+    let results = scaleBalance(input);
+    expect(results).to.equal('0,4');
+  });
+
+  it('should sort returned weights in ascending order', function() {
+    let input = ['[1, 9]', '[5, 3, 2]'];
+    let results = scaleBalance(input);
+    expect(results).to.equal('3,5');
+  });
+});
